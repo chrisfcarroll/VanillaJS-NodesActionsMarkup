941564747748aab028ac4761b116c38fdc913195
Chris F Carroll <chris.carroll@unforgettable.me.uk>
2023-09-08 18:07:59 +0100

    wip:computer-player

---
+++ ultimate-noughts-and-crosses/__tests__/models-computer-player.tests.js
@@ -0,0 +1,38 @@
+import OxoGame from '../js/Oxo-game'
+import UltimateOxoGame, {gameNumberFromName} from '../js/Ultimate-oxo-game'
+import {computerPlayMoveOnOxoGame, computerPlayMoveOnUltimateOxoGame} from '../js/computer-player'
+import ObservablePushQueue from '../js/Observable-push-queue'
+
+describe('Computer player', ()=>{
+
+  test('Can make valid moves Given a game', ()=>{
+
+    const moves=[]
+    const name=new Date().toISOString()
+    const game= new OxoGame(moves, name )
+
+    for(let i= 1; i <= 8 ; i++){
+      const player=game.playerOnMove
+      const playedAt= computerPlayMoveOnOxoGame(game)
+
+      expect(moves.length).toBe(i)
+      expect(moves[i-1]).toEqual({game:name, player:player, playedAt:playedAt})
+    }
+    console.log(moves)
+  })
+
+  test('Can make valid moves Given a metaGame', ()=>{
+
+    const moves=new ObservablePushQueue()
+    const name=new Date().toISOString()
+    const game= new UltimateOxoGame(moves)
+
+    const {board,playedAt}= computerPlayMoveOnUltimateOxoGame(game)
+
+    expect(moves.length).toBe(1)
+    expect(moves[0].playedAt).toBe(playedAt)
+    expect(gameNumberFromName( moves[0].game)).toBe(board)
+
+  })
+
+})
---
+++ ultimate-noughts-and-crosses/js/computer-player.js
@@ -0,0 +1,59 @@
+import {unplayedSquare} from './Oxo-game.js'
+
+const wins = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+            [1, 4, 7],
+            [2, 5, 8],
+            [3, 6, 9],
+            [1, 5, 9],
+            [3, 5, 7]
+        ]
+
+export function computerPlayMoveOnOxoGame(game){
+
+  const me= game.playerOnMove
+
+  const playables= wins
+          .map(line => {
+            const lineState={me:0, them:0, open:0, canPlayAt:0}
+            line.map(cell=> {
+              switch(game.boardModel[cell]){
+                case me: lineState.me+=1; break;
+                case unplayedSquare:
+                  lineState.open+=1;
+                  lineState.canPlayAt= lineState.canPlayAt || cell
+                  break;
+                default: lineState.them += 1;
+              }
+            })
+            return lineState
+          })
+          .filter(lineState=> lineState.canPlayAt)
+
+  playables.sort( (a,b) => a.open - b.open )
+  console.log(playables)
+
+  game.playMove( playables[0].canPlayAt )
+  return playables[0].canPlayAt
+}
+
+export function computerPlayMoveOnUltimateOxoGame(metaGame){
+
+  const me= metaGame.playerOnMove
+  const boardToPlay= (metaGame.nextBoard===0) ? chooseABoard() : metaGame.nextBoard
+
+  if(boardToPlay===0) return {board:0, cell:0}
+  return {
+    board: boardToPlay,
+    playedAt: computerPlayMoveOnOxoGame(metaGame.games[boardToPlay])
+  }
+
+  function chooseABoard(){
+    if(metaGame.nextBoard !== 0)return metaGame.nextBoard
+    ;
+    const playables= metaGame.games.filter(g => ! g.winLine)
+    return Math.ceil(Math.random() * playables.length)
+  }
+}
